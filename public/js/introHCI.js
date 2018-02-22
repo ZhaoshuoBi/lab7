a1b14b77862a495ba0fd3802d54808df6967f694
Zhaoshuo Bi <zbi@ucsd.com>
2018-02-21 21:49:52 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,8 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function() {
+		console.log("Like click");
+		ga("send", "event", "like", "click");
+	});
 }
